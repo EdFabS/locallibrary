c9a031f38432ef63da670caee0bf1ecc925c7cc0
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2019-01-04 22:48:40 -0600

    models created genre, book and book instance

--- catalogo/models.py
+++ catalogo/models.py
@@ -4,3 +4,75 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+	"""
+	Model que representa un género literario (p. ej. ciencia ficción, poesía, etc.).
+	"""
+	name = models.CharField(max_length=200, help_text="Enter a book genre (e.g Science Fiction, French Poety etc.)")
+
+	def __str__(self):
+		"""
+		cadena que representa a la instancia particular del modelo (p. ej en el sitio de Administracion)
+		"""
+		return self.name
+
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
+
+class Book(models.Model):
+	"""
+	Modelo que representa un libro (pero no un Ejemplar específico).
+	"""
+	title = models.CharField(max_length=200)
+
+	author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+	#ForeinKey, ya que un libro tiene un solo autor, pero el mismo autor puede haber escrito muchos libros.
+	#'Author' es un string, en vez de un objeto, por que la clase Author aun no ha sido declarada.
+
+	summary = models.TextField(max_length=1009, help_text="Enter a brief description of the book")
+
+	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn"> ISBN numbre</a>') 
+
+	genere = models.ManyToManyField(Genre, Help_text="Select a genre for his book")
+	# ManyToManyField, porque un género puede contener muchos linros y un libro puede cubrir varios géneros.
+	#La clase Genre ya ha sido definida, entonces podemos especificar el objeto arriba.
+
+	def __str__(self):
+		"""
+		String que representa al objeto Book
+		"""
+		return self.title
+
+	def get_absolute_url(self):
+		"""
+		Devuelve elURL a una instacia particular de Book
+		"""
+		return reverse('book-detail', args=[str(self.id)])
+
+import uuid # Requerida para las instancias de libros únicos
+
+class BookInstance(models.Model):
+	"""
+	Modelo que representa una copia específica de un libro (i.e. 	ue puede ser pewsentado por la biblioteca).
+	"""
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="ID único para este libro particular en toda la biblioteca")
+	book = model.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+	imprint =models.CharField(max_length=200)
+	due_back = models.DateField(null=True, blank=True)
+
+	LOAN_STATUS = (
+		('m', 'Maintence'),
+		('o', 'On loan'),
+		('a', 'Availabe'),
+		('r', 'Reserved'),
+	)
+
+	status = models.CharField(max_length=1,	choices=LOAN_STATUS, blank=True, default='m', help_text='Disponibilidad del libro')
+
+	class Meta:
+		ordering = ["due_back"]
+
+	def __str__(self):
+		"""
+		String para representar el Objeto del Modelo
+		"""
+		return '%s (%s)' % (self.id,self.book.title)
