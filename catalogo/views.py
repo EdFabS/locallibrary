7424d213d14cfce7b2440ecaaa7989c8e2f7946c
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2019-01-14 21:36:08 -0600

    i created the template with th template base and add the static files like style.css

--- catalogo/views.py
+++ catalogo/views.py
@@ -21,7 +21,7 @@ def index(request):
 	return render(
 		request,
 		'index.html',
-		context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_availables,'num_authors':num_authors},
+		context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
 	)
 
 def diferente(request):
