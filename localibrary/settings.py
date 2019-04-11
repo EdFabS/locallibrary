6f5bc9fc1e169ec1909b7d1f001f845b4dd43879
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2019-04-10 19:58:29 -0500

    Added files and changes required for deployment to heroku

--- localibrary/settings.py
+++ localibrary/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4=tu*z7ili*+mye@r*+sieo)d12+g4g#p14=*g=kea4=hxlt6p'
+#SECRET_KEY = '4=tu*z7ili*+mye@r*+sieo)d12+g4g#p14=*g=kea4=hxlt6p'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '4=tu*z7ili*+mye@r*+sieo)d12+g4g#p14=*g=kea4=hxlt6p')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 #ALLOWED_HOSTS = ['198.168.0.103', 'localhost', '127.0.0.1']
@@ -43,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,3 +128,12 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+#Simplified static file serving
+#https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
